a1c0032f31e83b37f0b73a32af260e605f5180c2
soma <soma@c59f2a53-1f6a-49bd-a20d-d3124f46c12c>
2013-01-27 01:34:50 +0000

    meshkit: add wr841 and wgt634u to modellist
    git-svn-id: http://svn.augsburg.freifunk.net/meshkit@706 c59f2a53-1f6a-49bd-a20d-d3124f46c12c

--- models/modellist.py
+++ models/modellist.py
@@ -4,18 +4,23 @@ config_modellist = {
     "Linksys":{
         "WRT54G":{"target":"brcm47xx", "profile":"Broadcom-b43"},
     },
+    "Netgear": {
+        "WGT634U":{"target":"brcm47xx", "profile":"WGT634U"},
+    },
     "Siemens":{
         "SE505v2":{"target":"brcm47xx", "profile":"Broadcom-b43"},
     },
-    "Ubiquiti":{
-        "Routerstation":{"target":"ar71xx", "profile":"UBNTRS"},
-        "Routerstation Pro":{"target":"ar71xx", "profile":"UBNTRSPRO"},
-    },
     "\"TP-Link\"":{
         "WR741":{"target":"ar71xx", "profile":"TLWR741"},
+        "WR841":{"target":"ar71xx", "profile":"TLWR841"},
         "WR1043ND":{"target":"ar71xx", "profile":"TLWR1043"},
         "WDR3600":{"target":"ar71xx", "profile":"TLWDR4300"},
         "WDR4300":{"target":"ar71xx", "profile":"TLWDR4300"},
         "WDR4310":{"target":"ar71xx", "profile":"TLWDR4300"},
     },
+    "Ubiquiti":{
+        "Routerstation":{"target":"ar71xx", "profile":"UBNTRS"},
+        "Routerstation Pro":{"target":"ar71xx", "profile":"UBNTRSPRO"},
+    },
+
 }
