289edb7c4df8f4d655d5b7dca7b009568445c0f8
soma <soma@c59f2a53-1f6a-49bd-a20d-d3124f46c12c>
2013-03-28 19:54:11 +0000

    meshkit: Fix restart of build queue
    git-svn-id: http://svn.augsburg.freifunk.net/meshkit@762 c59f2a53-1f6a-49bd-a20d-d3124f46c12c

--- init/build_queue.py
+++ init/build_queue.py
@@ -2,9 +2,9 @@ import sys
 import os
 import subprocess
 sys.path.append(os.path.join(request.folder, "private", "modules"))
-import utils
+import mkutils
 
-if utils.check_pid(os.path.join(request.folder, "private", "buildqueue.pid"), False):
+if mkutils.check_pid(os.path.join(request.folder, "private", "buildqueue.pid"), False):
     pass
 else:
     subprocess.Popen(['python', 'web2py.py', '-S', 'meshkit', '-M', '-N',  '-R', os.path.join(request.folder, 'private', 'build_queue.py')])
