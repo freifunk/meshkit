cb8a56c730ec5d6a5a02c61eee3180f7a5315f5c
Manuel Munz <munz@comuno.net>
2015-07-01 19:11:30 +0200

    remove support for GAE in db.py, scheduler doesn't work on GAE and it was probably never used

--- models/db.py
+++ models/db.py
@@ -7,18 +7,7 @@
 # custom user login controls in navbar
 import custom_layout as custom
 
-if not request.env.web2py_runtime_gae:
-    # if NOT running on Google App Engine use SQLite or other DB
-    db = DAL('sqlite://storage.sqlite')
-else:
-    # connect to Google BigTable (optional 'google:datastore://namespace')
-    db = DAL('google:datastore')
-    # store sessions and tickets there
-    session.connect(request, response, db=db)
-    # or store session in Memcache, Redis, etc.
-    # from gluon.contrib.memdb import MEMDB
-    # from google.appengine.api.memcache import Client
-    # session.connect(request, response, db = MEMDB(Client()))
+db = DAL('sqlite://storage.sqlite')
 
 # by default give a view/generic.extension to all actions from localhost
 # none otherwise. a pattern can be 'controller/function.extension'
