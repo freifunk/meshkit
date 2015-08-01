a8b12f87e909eb7782068afa16ac8eabbc9df090
Manuel Munz <munz@comuno.net>
2015-08-01 12:45:38 +0200

    add languages/plural-de.py

---
+++ languages/plural-de.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+{
+# "singular form (0)": ["first plural form (1)", "second plural form (2)", ...],
+'hit': ['Hits'],
+'hour': ['Stunden'],
+'minute': ['Minuten'],
+'miss': ['Misses'],
+'second': ['Sekunden'],
+}
